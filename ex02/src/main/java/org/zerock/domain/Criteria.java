c905cae3cdec5792e844ac08cf5a0335f60bb48d
down <down@localhost>
2020-08-23 15:16:10 +0900

    init commit

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -1,5 +1,7 @@
 package org.zerock.domain;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,7 +14,7 @@ public class Criteria {
 
   private int pageNum;
   private int amount;
-  
+
   private String type;
   private String keyword;
 
@@ -24,9 +26,21 @@ public class Criteria {
     this.pageNum = pageNum;
     this.amount = amount;
   }
-  
+
   public String[] getTypeArr() {
-    
+
     return type == null? new String[] {}: type.split("");
   }
+
+	// UriComponentsBuilder를 이용하는 링크 생성
+	public String getListLink()
+	{
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+			.queryParam("pageNum", this.pageNum)
+			.queryParam("amount", this.getAmount())
+			.queryParam("type", this.getType())
+			.queryParam("keyword", this.getKeyword());
+
+		return builder.toUriString();
+	}
 }
