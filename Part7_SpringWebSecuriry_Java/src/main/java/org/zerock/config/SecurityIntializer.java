d5b19f996961d65fbb0d2cd692b41f6bae63d886
down <down@localhost>
2020-08-23 15:18:28 +0900

    init commit

---
+++ Part7_SpringWebSecuriry_Java/src/main/java/org/zerock/config/SecurityIntializer.java
@@ -0,0 +1,8 @@
+package org.zerock.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityIntializer extends AbstractSecurityWebApplicationInitializer
+{
+
+}
